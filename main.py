38e3c4d5cc5b4cc321e22b5b0bd8f9108ccae8fb
HSGamer <huynhqtienvtag@gmail.com>
2022-11-22 13:23:40 +0700

    mnist model

--- main.py
+++ main.py
@@ -4,38 +4,14 @@ import uuid
 
 import cv2
 import imutils
-import matplotlib.pyplot as plt
 import numpy as np
 import pytesseract as tesseract
+
+from matplotlib import pyplot as plt
 from fastapi import FastAPI, File, UploadFile
 from imutils import contours as cont
 
 app = FastAPI()
-
-
-def sort_contours(cnts, method):
-    # initialize the reverse flag and sort index
-    reverse = False
-    i = 0
-
-    # handle if we need to sort in reverse
-    if method == "right-to-left" or method == "bottom-to-top":
-        reverse = True
-
-    # handle if we are sorting against the y-coordinate rather than
-    # the x-coordinate of the bounding box
-    if method == "top-to-bottom" or method == "bottom-to-top":
-        i = 1
-
-    # construct the list of bounding boxes and sort them from top to
-    # bottom
-    boundingBoxes = [cv2.boundingRect(c) for c in cnts]
-    (cnts, boundingBoxes) = zip(*sorted(zip(cnts, boundingBoxes),
-                                        key=lambda b: b[1][i], reverse=reverse))
-
-    # return the list of sorted contours and bounding boxes
-    return (cnts, boundingBoxes)
-
 
 def imshow(title, image, width=800):
     cv2.imshow(title, imutils.resize(image, width=width))
@@ -47,9 +23,11 @@ def threshold(image, invert=False):
         image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
     # Thresholding the image
-    thresh, img_bin = cv2.threshold(image, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+    thresh, img_bin = cv2.threshold(
+        image, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
     # Invert the image
-    if invert: img_bin = 255 - img_bin
+    if invert:
+        img_bin = 255 - img_bin
     return img_bin
 
 
@@ -67,9 +45,11 @@ def find_boxes(image):
     kernel_length = np.array(img_bin).shape[1] // 40
 
     # A verticle kernel of (1 X kernel_length), to detect all the verticle lines.
-    verticle_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (1, kernel_length))
+    verticle_kernel = cv2.getStructuringElement(
+        cv2.MORPH_RECT, (1, kernel_length))
     # A horizontal kernel of (kernel_length X 1), to detect all the horizontal lines.
-    horizontal_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (kernel_length, 1))
+    horizontal_kernel = cv2.getStructuringElement(
+        cv2.MORPH_RECT, (kernel_length, 1))
 
     verticle_lines = morph(img_bin, verticle_kernel)
     horizontal_lines = morph(img_bin, horizontal_kernel)
@@ -91,23 +71,20 @@ def over_draw_boxes(img_bin):
 
 def recognize(img, detect_text):
     h, w, c = img.shape
-    if detect_text:
-        # images.append(axs[i, j].imshow(sharped_img))
-        text = tesseract.image_to_string(img, lang="vie")
-    else:
-        # Convert to the gray-scale
-        # gry = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        #
-        # # Down-sample
-        # gry = cv2.resize(gry, None, fx=(50/h), fy=(50/h))
-        # gry = cv2.addWeighted(
-        #     gry, 4, cv2.blur(gry, (25, 25)), -4, 375)
-        # # images.append(axs[i, j].imshow(sharped_img))
-        gry = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    
+    # Convert to the gray-scale
+    gry = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-        # Down-sample
-        gry = cv2.resize(gry, None, fx=(50 / h), fy=(50 / h))
-        gry = cv2.addWeighted(gry, 4, cv2.blur(gry, (25, 25)), -4, 350)
+    # Down-sample
+    gry = cv2.resize(gry, None, fx=(50 / h), fy=(50 / h))
+    gry = cv2.threshold(gry, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+    
+    cv2.imwrite("gray.jpg", gry)
+    
+    text = ""
+    if detect_text:
+        text = tesseract.image_to_string(gry, lang="vie")
+    else:
         text = tesseract.image_to_string(gry, config="digits")
 
     print(text)
@@ -126,11 +103,14 @@ def ocr(filename, csvName):
     boxes = find_boxes(img)
     boxes = over_draw_boxes(boxes)
 
-    contours, _ = cv2.findContours(boxes, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    contours, _ = cv2.findContours(
+        boxes, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     # _, contours, _ = cv2.findContours(boxes, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-    (contours, boundingBoxes) = cont.sort_contours(contours, method="left-to-right")
-    (contours, boundingBoxes) = cont.sort_contours(contours, method="top-to-bottom")
+    (contours, boundingBoxes) = cont.sort_contours(
+        contours, method="left-to-right")
+    (contours, boundingBoxes) = cont.sort_contours(
+        contours, method="top-to-bottom")
 
     idx = 0
     images = []
@@ -153,7 +133,8 @@ def ocr(filename, csvName):
             cy = int(M['m01'] / M['m00'])
             center = (cx, cy + 20)
             if idx != 0:
-                cv2.putText(img, str(idx), center, cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 255, 0), 3)
+                cv2.putText(img, str(idx), center,
+                            cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 255, 0), 3)
                 cv2.imwrite("cropped/0.jpg", img)
 
             # cropped cell img, idx,
@@ -161,8 +142,10 @@ def ocr(filename, csvName):
             if idx == 0:
                 cv2.imwrite("cropped/craft-" + str(idx) + ".jpg", cell)
             else:
-                resize = cv2.resize(cell, None, fx=0.5, fy=0.5, interpolation=cv2.INTER_LINEAR)
-                cv2.imwrite("cropped/row-" + str(i) + "-col-" + str(j) + ".jpg", resize)
+                resize = cv2.resize(cell, None, fx=0.5,
+                                    fy=0.5, interpolation=cv2.INTER_LINEAR)
+                cv2.imwrite("cropped/row-" + str(i) +
+                            "-col-" + str(j) + ".jpg", resize)
                 j += 1
             # imshow("image", img)
             idx += 1
@@ -181,49 +164,7 @@ def ocr(filename, csvName):
     for i in range(a):
         cols = []
         for j in range(4):
-
             img = cv2.imread(f"cropped/row-{i}-col-{j}.jpg")
-
-            # h, w, c = img.shape
-            # rate = h/w
-
-            # if (j == 0):
-            #     gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-            #     gray = cv.threshold(gray, 0, 255,cv.THRESH_BINARY |cv.THRESH_OTSU)[1]
-            #     filename = "{}.png".format(os.getpid())
-            #     cv.imwrite(filename, gray)
-            #     images.append(axs[i, j].imshow(img))
-            #     text = tesseract.image_to_string(Image.open(filename),lang='vie')
-            #     os.remove(filename)
-            # else:
-
-            # resized_img = cv2.resize(img, None, fx=(50/h), fy=(50/h), interpolation=cv2.INTER_LINEAR)
-
-            # h, w, c = resized_img.shape
-
-            # print(f"{w} {h}")
-
-            # sharped_img = cv2.addWeighted(
-            #     img, 4, cv2.blur(img, (25, 25)), -4, 350)
-
-            # gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-            # gray = cv.threshold(gray, 0, 255, cv.THRESH_BINARY | cv.THRESH_OTSU)[1]
-            # filename = "{}.png".format(os.getpid())
-            # cv.imwrite(filename, gray)
-            #
-            # images.append(axs[i, j].imshow(sharped_img))
-            #
-            # text = tesseract.image_to_string(Image.open(filename),config='--psm 12 --oem 3', lang='vie')
-            # os.remove(filename)
-            # gry = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-            #
-            # # Down-sample
-            # gry = cv2.resize(gry, None, fx=(50 / h), fy=(50 / h))
-            # gry = cv2.addWeighted(
-            #     gry, 4, cv2.blur(gry, (25, 25)), -4, 350)
-            # images.append(axs[i, j].imshow(sharped_img))
-            # text = tesseract.image_to_string(gry, config="digits")
-
             text = recognize(img, j == 0)
 
             if len(text) == 0 and j > 0:
@@ -272,3 +213,11 @@ async def create_upload_file(file: UploadFile = File(...)):
     #     print("The csv does not exist")
 
     return list
+
+@app.post("/recognize/")
+async def test_recognize(file: UploadFile = File(...)):
+    contents = await file.read()
+    nparr = np.fromstring(contents, np.uint8)
+    img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+    predict = recognize(img, False)
+    return predict
